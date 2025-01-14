9dd8e19ce0eb213a327e223774364dcb0fba0620
Vy Vu <74190308+bellavee@users.noreply.github.com>
2025-01-14 12:03:04 +0100

    Update Ball.h

--- LightEngine/Ball.h
+++ LightEngine/Ball.h
@@ -7,6 +7,7 @@ class Ball : public Entity
 {
 public:
 	void OnUpdate() override;
+	void MoveTo(Player* player);
 
 	Player* GetCurrentHolder() const { return mCurrentHolder; }
 	void SetCurrentHolder(Player* player) { mCurrentHolder = player; }
